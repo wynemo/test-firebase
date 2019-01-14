aa372b6d8490cd0de006c2b221c37ee4aa3a3994
Nemo Zhang <nemo@nemo.zencoo.com>
2019-01-14 12:22:50 -0500

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: '534678906736' // troque pelo seu sender id 
+});
+
+const messaging = firebase.messaging();
+
